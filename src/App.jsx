a4de78a0bf1c672cb968fc6cb6159c9782d8fb47
quocnguyenphv <quoc.nguyen@jrgvn.com>
2022-08-04 13:50:25 +0700

    update appbar and container UI in layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Layout from "./components/Layout";
 import Home from "./pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,33 +2,49 @@ import { AppBar, Box, Container, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff",
+  },
+  appBarLinks: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginLeft: "1rem",
+    gap: "1rem",
+  },
+  bodyBox: {
+    backgroundImage: `url("https://uat2.pizzahut.vn/static/media/background.8c532c6143e1b30fc4a3.jpg")`,
+  },
+  mainContainer: {
+    maxWidth: "960px",
+    minHeight: "85vh",
+  },
+}));
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h5" component="h1">
-            Hello
+          <Typography variant="h5" component="h1" color="primary">
+            PizzaHut
           </Typography>
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginLeft: "1rem",
-              gap: "1rem",
-            }}
-          >
+          <Box className={classes.appBarLinks}>
             <Link to="/">Home</Link>
             <Link to="/coupon">Coupon</Link>
           </Box>
         </Toolbar>
       </AppBar>
-      <Container style={{ minHeight: "85vh" }}>{children}</Container>
+      <Box className={classes.bodyBox}>
+        <Container className={classes.mainContainer}>{children}</Container>
+      </Box>
       <footer>
         <Box>
           <Typography variant="body2" style={{ textAlign: "center" }}>
-            Hello 2022
+            PizzaHut 2022
           </Typography>
         </Box>
       </footer>
